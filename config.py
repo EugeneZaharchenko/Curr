11df4892733fb6abfe9a03a890a5d98d63830c56
eugen <eugenezaharchenko@gmail.com>
2020-02-21 00:25:22 +0200

    refactored from lesson 6

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ DB_NAME = "golden-eye.db"
 
 LOGGER_CONFIG = dict(level=logging.DEBUG,
                      file="app.log",
-                     formatter=logging.Formatter("%(asctime)s [%(levelname)s] - %(name)s:%(message)s")
+                     formatter=logging.Formatter(u"%(asctime)s [%(levelname)s] - %(name)s:%(message)s")
                      )
+
+HTTP_TIMEOUT = 15
