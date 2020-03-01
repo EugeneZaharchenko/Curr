fefeb0ad712acebb859970479cdb68570f513868
J0nnyZ <EugeneZaharchenko@users.noreply.github.com>
2020-03-02 00:09:45 +0200

    Restructured. Tests need to be fixed

--- tests.py
+++ tests.py
@@ -1,9 +1,10 @@
 import unittest
 
 import api.test_api as test_api
-import models
 import api.privat_api as privat_api
-import api.cbr_api as cbr_api
+
+import api
+import models
 
 
 class Test(unittest.TestCase):
@@ -25,23 +26,49 @@ class Test(unittest.TestCase):
         xrate = models.XRate.get(id=1)
         updated_before = xrate.updated
         self.assertEqual(xrate.rate, 1.0)
-        privat_api.update_xrates(840, 980)
+
+        privat_api.Api().update_rate(840, 980)
+
         xrate = models.XRate.get(id=1)
         updated_after = xrate.updated
 
         self.assertGreater(xrate.rate, 24)
         self.assertGreater(updated_after, updated_before)
 
+        api_log = models.ApiLog.select().order_by(models.ApiLog.created.desc()).first()
+
+        self.assertIsNotNone(api_log)
+        self.assertEqual(api_log.request_url, "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11")
+        self.assertIsNotNone(api_log.response_text)
+
+        self.assertIn('[{"ccy": "USD", "base_ccy": "UAH"}]', api_log.response_text)
+
+    def test_privat_currency_error(self):
+        xrate = models.XRate.get(id=1)
+        self.assertEqual(xrate.rate, 1.0)
+
+        self.assertRaises(ValueError, privat_api.Api)
+
+    # TODO: test not working
     def test_cbr(self):
         xrate = models.XRate.get(from_currency=840, to_currency=643)
         updated_before = xrate.updated
         self.assertEqual(xrate.rate, 1.0)
-        cbr_api.Api().update_rate(840, 643)
+
+        api.update_rate(840, 643)
+
         xrate = models.XRate.get(from_currency=840, to_currency=643)
         updated_after = xrate.updated
 
         self.assertGreater(xrate.rate, 60)
         self.assertGreater(updated_after, updated_before)
+
+        api_log = models.ApiLog.select().order_by(models.ApiLog.created.desc()).first()
+
+        self.assertIsNotNone(api_log)
+        self.assertEqual(api_log.request_url, "http://www.cbr.ru/scripts/XML_daily.asp")
+        self.assertIsNotNone(api_log.response_text)
+        self.assertIn("<NumCode>840</NumCode>", api_log.response_text)
 
 
 if __name__ == '__main__':
